8e614b3ff2b13b2f2eadbf4274bccc73c237132a
Ammwal <bismakhan7334@gmail.com>
2025-03-03 21:43:32 +0500

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://promange-app-frontend.onrender.com",
+    origin: "https://promange-app-frontend.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
